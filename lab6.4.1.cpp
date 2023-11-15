4e0b030ac4c42c0e1e0764da9057612e9030f5bb
LizaKuzminska <yelyzavetakuzminska@gmail.com>
2023-11-15 20:52:45 +0200

    fvsfv

---
+++ lab6.4.1.cpp
@@ -0,0 +1,67 @@
+﻿// lab6.4.1.cpp
+// Кузьмінська Єлизавета
+// Лабораторна робота № 6.4
+//  Опрацювання та впорядкування одновимірних динамічних масивів
+// Варіант 10
+
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void Create(int* a, const int n, int Low, int High)
+{
+	for (int i = 0; i < n; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+int Min(int* a, const int n) {
+	int min = abs(a[0]);
+	for (int i = 0; i < n; i++)
+		if (abs(a[i]) < min)
+			min = abs(a[i]);
+	return min;
+}
+int Sum(int* a, const int n) {
+	int S = 0;
+	int m;
+	for (int i = 0; i < n; i++)
+		if (a[i] == 0) {
+			m = i;
+			for (m = i; m < n; m++) {
+				S += a[m];
+			}
+		}
+	return S;
+}
+void Masuv(int* a, const int n) {
+	for (int i = 0; i < n; i += 2)
+		cout << a[i] << " ";
+	for (int i = 1; i < n; i += 2)
+		cout << a[i] << " ";
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	int n;
+	cout << "n="; cin >> n;
+	int* a = new int[n];
+	int Low;
+	cout << "Low="; cin >> Low;
+	int High;
+	cout << "High="; cin >> High;
+	Create(a, n, Low, High);
+	Print(a, n);
+	cout << " min = " << Min(a, n) << endl;
+	cout << " Sum = " << Sum(a, n) << endl;
+	//cout << "Masuv = " << Masuv(a, n) << endl;
+	Masuv(a, n);
+	return 0;
+}
